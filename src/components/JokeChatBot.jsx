60382f5d780478529e32c30e07a32fda87396851
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2022-09-04 23:31:22 -0500

    cambio de api de jokes

--- src/components/JokeChatBot.jsx
+++ src/components/JokeChatBot.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { fetchJoke } from '../fetch/fetchJoke';
 import { LoadingIcon } from './LoadingIcon';
@@ -8,7 +9,8 @@ export const JokeChatBot = () => {
         joke: '',
         loading: true
     });
-    
+
+
 
     useEffect(() => {
         const getJ = async() => {
--- src/fetch/fetchJoke.js
+++ src/fetch/fetchJoke.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export const fetchJoke = async() => {
-    const { data } = await axios.get('https://api.icndb.com/jokes/random');
-    return data.value.joke;
+    const { data } = await axios.get('https://geek-jokes.sameerkumar.website/api?format=json');
+    return data.joke;
 }
